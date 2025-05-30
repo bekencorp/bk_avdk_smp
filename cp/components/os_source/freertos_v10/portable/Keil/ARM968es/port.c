49a9f7b4268a099b314803ee3f6b759c29e2f5f6
itester <itester@aclsemi.com>
2025-05-30 20:18:38 +0800

    [CI] [60713]  <pm> <perf>when vTaskSuspendAll, then call rtos_delay will cause assert

--- cp/components/os_source/freertos_v10/portable/Keil/ARM968es/port.c
+++ cp/components/os_source/freertos_v10/portable/Keil/ARM968es/port.c
@@ -124,6 +124,19 @@ void vApplicationIdleHook( void )
 	extern void bk_task_wdt_feed(void);
 	bk_task_wdt_feed();
 #endif
+#if (CONFIG_CPU_CNT > 1)
+	uint32_t cp1_psram_malloc_count_state       = 0;
+	if(bk_pm_low_vol_vote_state_get())
+	{
+		cp1_psram_malloc_count_state = bk_pm_get_cp1_psram_malloc_count();
+		pm_cp1_psram_malloc_count_state_set(cp1_psram_malloc_count_state);
+		//pm_debug_module_state();
+	}
+	if(bk_pm_cp1_recovery_all_state_get())
+	{
+		bk_pm_module_check_cp1_shutdown();
+	}
+#endif
 }
 
 /*-----------------------------------------------------------*/
