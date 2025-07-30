b7eb44a3e5a1655456c88842e71df1b584bfc9fd
itester <itester@aclsemi.com>
2025-07-30 18:56:08 +0800

    [CI] [65044]  <wifi> <feat> porting ppp to smp branch

--- ap/components/lwip_intf_v2_1/lwip-2.1.2/src/apps/ping/ping.c
+++ ap/components/lwip_intf_v2_1/lwip-2.1.2/src/apps/ping/ping.c
@@ -420,24 +420,24 @@ int ping(char* target_name, uint32_t times, size_t size)
             if ((recv_len = ping_mid_recv(s, &ttl,af)) >= 0)
             {
                 recv_cnt++;
-                LWIP_DEBUGF( PING_DEBUG, ("%d bytes from %s icmp_seq=%d ttl=%d time=%d ticks\n", recv_len, ipaddr_ntoa(&target_addr), send_times,
-                        ttl, sys_now() - recv_start_tick));
+                LWIP_LOGI("%d bytes from %s icmp_seq=%d ttl=%d time=%d ticks\n", recv_len, ipaddr_ntoa(&target_addr), send_times,
+                        ttl, sys_now() - recv_start_tick);
             }
             else
             {
-				LWIP_DEBUGF( PING_DEBUG, ("From %s icmp_seq=%d timeout\n", ipaddr_ntoa(&target_addr), send_times));
+				LWIP_LOGI("From %s icmp_seq=%d timeout\n", ipaddr_ntoa(&target_addr), send_times);
             }
         }
         else
         {
-			LWIP_DEBUGF( PING_DEBUG, ("Send %s - error\n", ipaddr_ntoa(&target_addr)));
+			LWIP_LOGI("Send %s - error\n", ipaddr_ntoa(&target_addr));
         }
 
         send_times++;
         if (send_times >= times)
         {
-			LWIP_DEBUGF( PING_DEBUG, ("ping end, sent cnt: %d, recv cnt: %d, drop cnt: %d(%.1f%%)\n", 
-				sent_cnt, recv_cnt, (times-recv_cnt), (float)(((times-recv_cnt)*100)/times)));
+			LWIP_LOGI("ping end, sent cnt: %d, recv cnt: %d, drop cnt: %d(%.1f%%)\n", 
+				sent_cnt, recv_cnt, (times-recv_cnt), (float)(((times-recv_cnt)*100)/times));
             /* send ping times reached, stop */
             break;
         }
