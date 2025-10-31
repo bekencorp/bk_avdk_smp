65a2d709b734c8c68ce51d3668311a74fc515e31
itester <itester@aclsemi.com>
2025-10-31 16:27:15 +0800

    [CI] [75654]  <platform> <feat> Add dwtf cmd for cpu freq

--- ap/components/bk_cli/cli_dwt.c
+++ ap/components/bk_cli/cli_dwt.c
@@ -3,6 +3,7 @@
 
 #if CONFIG_SOC_BK7236XX
 #include "dwt.h"
+extern void bk_delay_us(UINT32 us);
 
 static void dwt_command_usage(void)
 {
@@ -22,6 +23,8 @@ static void dwt_command_usage(void)
     BK_LOGD(NULL, "dwtdd data_address data_value\r\n");
     BK_LOGD(NULL, "     data_address: watch this data address, hex format\r\n");
     BK_LOGD(NULL, "     data_value: match the data value with watching this data address, hex format\r\n");
+    BK_LOGD(NULL, "dwtf ms\r\n");
+    BK_LOGD(NULL, "     ms: measure window in milliseconds using bk_delay_us()\r\n");
 }
 
 static void dwti_Command(char *pcWriteBuffer, int xWriteBufferLen, int argc, char **argv)
@@ -97,6 +100,41 @@ static void dwtdr_Command(char *pcWriteBuffer, int xWriteBufferLen, int argc, ch
     dwt_set_data_address_range(addr, limit, ACCESS_TYPE_WRITE);
 }
 
+static void dwtf_Command(char *pcWriteBuffer, int xWriteBufferLen, int argc, char **argv)
+{
+    uint32_t measure_ms;
+    uint32_t start, end;
+    uint32_t cycles;
+    uint64_t freq_hz;
+    uint32_t freq_mhz;
+
+    if (2 != argc) {
+        dwt_command_usage();
+        return;
+    }
+
+    measure_ms = strtoul(argv[1], NULL, 10);
+    if (measure_ms == 0U) {
+        BK_LOGD(NULL, "invalid ms: %s\r\n", argv[1]);
+        return;
+    }
+
+    dwt_init_cycle_counter();
+
+    start = dwt_get_cycle_counter_val();
+    bk_delay_us(measure_ms * 1000U);
+    end = dwt_get_cycle_counter_val();
+
+    cycles = end - start; /* unsigned handles wrap-around */
+
+    /* freq_hz = cycles / (measure_ms/1000.0) = cycles * 1000 / measure_ms */
+    freq_hz = ((uint64_t)cycles * 1000ULL) / (uint64_t)measure_ms;
+
+    freq_mhz = (uint32_t)(freq_hz / 1000000ULL);
+    BK_LOGD(NULL, "dwtf: ms=%u, cycles=%u, freq=%u MHz\r\n",
+            measure_ms, cycles, freq_mhz);
+}
+
 
 #define DWT_CMD_CNT (sizeof(s_dwt_commands) / sizeof(struct cli_command))
 static const struct cli_command s_dwt_commands[] = {
@@ -104,6 +142,7 @@ static const struct cli_command s_dwt_commands[] = {
     {"dwtd", "dwtd r/w/b data_address", dwtd_Command},
     {"dwtdr", "dwtdr data_address data_address_limit", dwtdr_Command},
     {"dwtdd", "dwtdd data_address data_value", dwtdd_Command},
+    {"dwtf", "dwtf ms (measure CPU freq)", dwtf_Command},
 };
 
 int cli_dwt_init(void)
