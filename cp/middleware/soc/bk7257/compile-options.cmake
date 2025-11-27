46a1108128bf9e0ce51966eae0bf3e8f31783040
itester <itester@aclsemi.com>
2025-11-27 16:17:37 +0800

    [CI] [77956]  <platform> <feat> wrapper the calloc_r func and update the cmake(cp mid part)

--- cp/middleware/soc/bk7257/compile-options.cmake
+++ cp/middleware/soc/bk7257/compile-options.cmake
@@ -6,3 +6,22 @@ set(OVERRIDE_COMPILE_OPTIONS
     "-fstack-protector"
 )
 
+set(OVERRIDE_LINK_OPTIONS
+    "-fno-builtin-printf"
+    "-Wl,--gc-sections"
+    "-Wl,--wrap=malloc"
+    "-Wl,--wrap=_malloc_r"
+    "-Wl,--wrap=free"
+    "-Wl,--wrap=_free_r"
+    "-Wl,--wrap=zalloc"
+    "-Wl,--wrap=calloc"
+    "-Wl,--wrap=calloc_r"
+    "-Wl,--wrap=realloc"
+    "-Wl,--wrap=_realloc_r"
+    "-Wl,--wrap=printf"
+    "-Wl,--wrap=vsnprintf"
+    "-Wl,--wrap=snprintf"
+    "-Wl,--wrap=sprintf"
+    "-Wl,--wrap=puts"
+    "-Wl,--wrap=__assert_func"
+)
