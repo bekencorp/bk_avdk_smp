95e9d9a2effa25364b88993ace29736ffcbf5e10
itester <itester@aclsemi.com>
2025-12-13 11:02:36 +0800

    [CI] [79336,79337]  <multimedia> <refactor> delete useless cli code

--- ap/components/bk_cli/cli_config.h
+++ ap/components/bk_cli/cli_config.h
@@ -144,12 +144,6 @@ extern "C" {
 #define CLI_CFG_AON_RTC     0
 #endif
 
-#if (CONFIG_JPEGDEC_SW)
-#define CLI_CFG_JPEGDEC     0
-#else
-#define CLI_CFG_JPEGDEC     0
-#endif
-
 #if (CONFIG_CALENDAR)
 #define CLI_CFG_CALENDAR    1
 #else
@@ -199,30 +193,6 @@ extern "C" {
 #define CLI_CFG_IPERF       1
 #else
 #define CLI_CFG_IPERF       0
-#endif
-
-#if (CONFIG_LCD_TEST)
-#define CLI_CFG_LCD  0
-#else
-#define CLI_CFG_LCD  0
-#endif
-
-#if (CONFIG_HW_ROTT_TEST)
-#define CLI_CFG_ROTT  0
-#else
-#define CLI_CFG_ROTT  0
-#endif
-
-#if (CONFIG_LCD_QSPI_TEST)
-#define CLI_CFG_LCD_QSPI	1
-#else
-#define CLI_CFG_LCD_QSPI	0
-#endif
-
-#if (CONFIG_DMA2D_TEST)
-#define CLI_CFG_DMA2D  0
-#else
-#define CLI_CFG_DMA2D  0
 #endif
 
 #if (CONFIG_SOC_BK7236XX) || (CONFIG_SOC_BK7239XX) || (CONFIG_SOC_BK7286XX)
