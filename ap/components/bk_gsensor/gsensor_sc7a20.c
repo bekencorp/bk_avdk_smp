8acbf50365c21207d0cb2b6dce9577f7caafe56d
itester <itester@aclsemi.com>
2025-12-13 11:03:01 +0800

    [CI] [79281]  <platform> <fix> restore interrupt latch to default mode

--- ap/components/bk_gsensor/gsensor_sc7a20.c
+++ ap/components/bk_gsensor/gsensor_sc7a20.c
@@ -618,15 +618,12 @@ static int sc7a20_open(void)
         sc7a20_i2c_write(SL_SC7A20_CTRL_REG2,0x81);
         sc7a20_i2c_write(SL_SC7A20_CTRL_REG3,0x40);
         sc7a20_i2c_write(SL_SC7A20_CTRL_REG4,0x98);
-        sc7a20_i2c_write(SL_SC7A20_CTRL_REG5,0x08); //Enable Latch interrupt
+        sc7a20_i2c_write(SL_SC7A20_CTRL_REG5,0x00);
         sc7a20_i2c_write(SL_SC7A20_CTRL_REG6,0x02);
         sc7a20_i2c_write(SL_SC7A20_SDOI2C_PU_CFG,0xc);
         sc7a20_i2c_write(SL_SC7A20_INT1_CFG,0xaa);//Draw an similar S-shaped gesture to wake up
         sc7a20_i2c_write(SL_SC7A20_INT1_THS,0x05);//Threshold value
         sc7a20_i2c_write(SL_SC7A20_INT1_DURATION,0x02);
-
-        unsigned char dummy;
-        sc7a20_i2c_read(SL_SC7A20_INT1_SRC, 1, &dummy); //Clear interrupt
 
         bk_gpio_enable_interrupt(GSENSOR_G_INT_PIN);
     }
