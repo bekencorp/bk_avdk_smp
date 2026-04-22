9ffad1b4ddd997d74b8d117fb8ca1ed8382c3219
itester <itester@aclsemi.com>
2026-04-22 15:04:34 +0800

    [CI] [87154]  <wifi> <fix> thread cmd update

--- cp/components/bk_cli/cli_mac802154.c
+++ cp/components/bk_cli/cli_mac802154.c
@@ -6,13 +6,14 @@
 extern void cli_mac802154(char *pcWriteBuffer, int xWriteBufferLen, int argc, char **argv);
 extern void txdtm_mac80154_cmd(char *pcWriteBuffer, int xWriteBufferLen, int argc, char **argv);
 extern void rxdtm_mac80154_cmd(char *pcWriteBuffer, int xWriteBufferLen, int argc, char **argv);
-
+extern void thread_mac802154_cmd(char *pcWriteBuffer, int xWriteBufferLen, int argc, char **argv);
 
 #define CLI_CMD_CNT (sizeof(s_mac802154_commands) / sizeof(struct cli_command))
 static const struct cli_command s_mac802154_commands[] = {
     {"mac802154",   "mac802154 [tx|rx] [channel] [tx_cont|rx_mode]",cli_mac802154},
-    {"txdtm",       "txdtm [-c] [-s] [-h]",                         txdtm_mac80154_cmd},
-    {"rxdtm",       "rxdtm ",                                       rxdtm_mac80154_cmd},
+    {"thread",      "thread dut | thread exit", thread_mac802154_cmd},
+    {"txthread",    "txthread -h | -c ch [-n n -f type -t mode] | -p idx | -r | -stop", txdtm_mac80154_cmd},
+    {"rxthread",    "rxthread -h | -c ch | -g 0 | -r | -stop", rxdtm_mac80154_cmd},
 };
 
 int cli_mac802154_init(void)
