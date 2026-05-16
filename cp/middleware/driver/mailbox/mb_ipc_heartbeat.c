f5c7376581bcefb8f6992388f9c9ad864657ba90
itester <itester@aclsemi.com>
2026-05-16 18:01:10 +0800

    [CI] [90212]  <platform> <feat> change the priority of heart_beat (cp/middleware part)

--- cp/middleware/driver/mailbox/mb_ipc_heartbeat.c
+++ cp/middleware/driver/mailbox/mb_ipc_heartbeat.c
@@ -20,6 +20,7 @@
 #include <modules/pm.h>
 
 #define MOD_TAG		"hrt"
+#define BEKEN_HEARTBEAT_PRIORITY 1
 
 #if (CONFIG_CPU_CNT > 1)
 
@@ -414,7 +415,7 @@ bk_err_t mb_ipc_heartbeat_init(void)
 	bk_err_t	ret_val = BK_FAIL;
 
 #if defined(MASTER_HB_TASK) || defined(SLAVE_HB_TASK)
-	ret_val = rtos_create_thread(NULL, BEKEN_DEFAULT_WORKER_PRIORITY, "heartbeat", mb_ipc_task, 1024, 0);
+	ret_val = rtos_create_thread(NULL, BEKEN_HEARTBEAT_PRIORITY, "heartbeat", mb_ipc_task, 1024, 0);
 #endif
 
 	if(ret_val != BK_OK)
