024297ed784951964cfeb591c79266e080c9f34c
itester <itester@aclsemi.com>
2026-05-30 14:46:15 +0800

    [CI] [91230]  <bt> <perf>Increase low-voltage wakeup margin from 6.8ms to 7.4ms

--- cp/components/bk_bluetooth/soc/bk7259/bluetooth.c
+++ cp/components/bk_bluetooth/soc/bk7259/bluetooth.c
@@ -1226,7 +1226,7 @@ static void bt_delay_us(uint32_t us)
 
 static uint16_t bt_get_low_voltage_wakeup_margin_cycles(void)
 {
-    return 218;//about 6.8ms
+    return 236;//about 7.4ms
 }
 
 //warning: bt_osi_funcs must be data section, otherwise a2dp_source_pcm and a2dp_source_decode will trig watchdog !!!!!!!!
