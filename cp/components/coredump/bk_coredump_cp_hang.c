fd9cefc7c495ed01092f2d47447e20f89e12cb5a
itester <itester@aclsemi.com>
2026-06-09 19:35:17 +0800

    [CI] [92418]  <platform> <feat> Pause CP hang heartbeat while AP is powered off

--- cp/components/coredump/bk_coredump_cp_hang.c
+++ cp/components/coredump/bk_coredump_cp_hang.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <components/log.h>
 #include <driver/ipi_driver.h>
+#include <modules/pm.h>
 #include <os/os.h>
 
 #define CP_HANG_TAG "cp_hang"
@@ -19,7 +20,7 @@
 #define CP_HANG_AON_WDT_PERIOD_MS (CP_HANG_TIMEOUT_FALLBACK_MS + CP_HANG_TIMEOUT_MARGIN_MS)
 #endif
 
-static volatile uint8_t s_cp_hang_debug_heartbeat_paused;
+static volatile uint8_t s_cp_hang_ap_power_off;
 
 static uint16_t cp_hang_get_ap_timeout_ms(void)
 {
@@ -43,9 +44,37 @@ static uint16_t cp_hang_get_ap_timeout_ms(void)
 	return (uint16_t)timeout_ms;
 }
 
-void bk_cp_hang_debug_heartbeat_pause(bool pause)
+static void cp_hang_ap_poweroff_callback(void *arg)
 {
-	s_cp_hang_debug_heartbeat_paused = pause ? 1U : 0U;
+	(void)arg;
+
+	s_cp_hang_ap_power_off = 1U;
+	BK_LOGI(CP_HANG_TAG, "AP power off, pause CP hang heartbeat\r\n");
+}
+
+static void cp_hang_ap_poweron_callback(void *arg)
+{
+	(void)arg;
+
+	s_cp_hang_ap_power_off = 0U;
+	BK_LOGI(CP_HANG_TAG, "AP power on, resume CP hang heartbeat\r\n");
+}
+
+static void cp_hang_register_ap_power_callbacks(void)
+{
+	bk_err_t ret;
+
+	ret = bk_pm_ap_ctrl_callback_register(cp_hang_ap_poweroff_callback, NULL,
+		PM_AP_CTRL_CB_TYPE_POWER_OFF);
+	if (ret != BK_OK) {
+		BK_LOGW(CP_HANG_TAG, "register AP power off callback failed: %d\r\n", ret);
+	}
+
+	ret = bk_pm_ap_ctrl_callback_register(cp_hang_ap_poweron_callback, NULL,
+		PM_AP_CTRL_CB_TYPE_POWER_ON);
+	if (ret != BK_OK) {
+		BK_LOGW(CP_HANG_TAG, "register AP power on callback failed: %d\r\n", ret);
+	}
 }
 
 static void cp_hang_debug_heartbeat_task(void *param)
@@ -56,7 +85,7 @@ static void cp_hang_debug_heartbeat_task(void *param)
 
 	while (1) {
 		rtos_delay_milliseconds(CONFIG_CP_HANG_DUMP_BY_AP_PERIOD_MS);
-		if (s_cp_hang_debug_heartbeat_paused != 0U) {
+		if (s_cp_hang_ap_power_off != 0U) {
 			continue;
 		}
 		if (bk_ipi_send_domain(IPI_AP_CORE0, IPI_DOMAIN_CP_HANG_DEBUG,
@@ -69,6 +98,8 @@ static void cp_hang_debug_heartbeat_task(void *param)
 bk_err_t bk_cp_hang_debug_heartbeat_init(void)
 {
 	bk_err_t ret;
+
+	cp_hang_register_ap_power_callbacks();
 
 	ret = rtos_create_thread(NULL, CP_HANG_HEARTBEAT_PRIORITY,
 		"cp_hang_hb", cp_hang_debug_heartbeat_task,
