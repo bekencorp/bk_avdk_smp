957bbfed8863e2ba3e58987d956ce2d596886037
itester <itester@aclsemi.com>
2026-06-09 19:48:58 +0800

    [CI] [92623]  <platform> <fix> avoid CP TRNG access when disabled

--- cp/components/bk_system/bk_platform.c
+++ cp/components/bk_system/bk_platform.c
@@ -43,90 +43,9 @@ int bk_rand(void)
 
 	return (number & RAND_MAX);
 }
-#else
-#define TRNG_READ_COUNT       (16)
-
-static bool s_trng_driver_is_init = false;
-
-#define TRNG_READ_REG(offset)       *(volatile uint32_t *)(SOC_TRNG_REG_BASE + (offset))
-#define TRNG_WRITE_REG(offset, val)  (*(volatile uint32_t *)(SOC_TRNG_REG_BASE + (offset))) = (uint32_t)(val)
-#define TRNG_CLKRST_REG_OFFSET       (0x02)
-#define TRNG_CONFIG_REG_OFFSET       (0x04)
-#define TRNG_DATA_REG_OFFSET         (0x05)
-
-static void trng_init_common(void)
-{
-    uint32_t val;
-
-    TRNG_WRITE_REG(TRNG_CLKRST_REG_OFFSET * 4, 0x1);
-
-    val = TRNG_READ_REG(TRNG_CONFIG_REG_OFFSET * 4);
-    TRNG_WRITE_REG(TRNG_CONFIG_REG_OFFSET * 4, val | 0x1);
-    s_trng_driver_is_init = true;
-}
-
-static void trng_deinit_common(void)
-{
-	uint32_t val;
-
-	s_trng_driver_is_init = false;
-	val = TRNG_READ_REG(TRNG_CONFIG_REG_OFFSET * 4);
-	TRNG_WRITE_REG(TRNG_CONFIG_REG_OFFSET * 4, val & (~0x1));
-}
-
-#define TRNG_INIT_IF_UNINITALIZED() do {\
-		if (!s_trng_driver_is_init) {\
-			trng_init_common();\
-		}\
-	} while(0)
-
-static uint32_t trng_get_random_number(void)
-{
-	TRNG_INIT_IF_UNINITALIZED();
-	return TRNG_READ_REG(TRNG_DATA_REG_OFFSET * 4);
-}
-
-static bk_err_t bk_trng_start(void)
-{
-	trng_init_common();
-	return BK_OK;
-}
-
-static bk_err_t bk_trng_stop(void)
-{
-	trng_deinit_common();
-	return BK_OK;
-}
-
-static void trng_delay_us(uint32 num)
-{
-	volatile uint32 i, j, us_count;
-
-	us_count = 4;
-	for (i = 0; i < num; i++){
-		for (j = 0; j < us_count; j++){
-                        ;
-                }
-        }
-}
-
+#elif !CONFIG_TRNG_SUPPORT
 int bk_rand(void)
 {
-	int i, number;
-
-	bk_trng_start();
-
-	trng_delay_us(50);  //add delay to make trng disckg take effect
-	/*Different board , same time point, the trng generate random number maybe same*/
-	for(i = 0; i < TRNG_READ_COUNT; i++) {
-		trng_get_random_number();
-	}
-
-	number = (int)trng_get_random_number();
-	bk_trng_stop();
-
-	return (number & RAND_MAX);
+	return rand();
 }
-
-#endif /* CONFIG_MBEDTLS_ACCELERATOR && CONFIG_SPE */
-
+#endif
