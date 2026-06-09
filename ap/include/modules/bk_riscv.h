1ad6a26f74dacb6f68a30df2c959c1e23b0c5af7
itester <itester@aclsemi.com>
2026-06-09 19:19:13 +0800

    [CI] [91871]  <multimedia> <feat> merge riscv swap struct define

--- ap/include/modules/bk_riscv.h
+++ ap/include/modules/bk_riscv.h
@@ -6,8 +6,11 @@ extern "C" {
 
 #include <common/bk_include.h>
 
-const unsigned char *bk_riscv_usb_host_fw_addr(void);
-const unsigned int bk_riscv_usb_host_fw_len(void);
+const unsigned char *bk_riscv_usb_fw_addr(void);
+const unsigned int   bk_riscv_usb_fw_len(void);
+
+static inline const unsigned char *bk_riscv_usb_host_fw_addr(void) { return bk_riscv_usb_fw_addr(); }
+static inline const unsigned int   bk_riscv_usb_host_fw_len(void)  { return bk_riscv_usb_fw_len(); }
 
 #ifdef __cplusplus
 }
