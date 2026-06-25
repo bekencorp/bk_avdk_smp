505fd1bf8ab3d875202c102265cd6afddaaf5f31
itester <itester@aclsemi.com>
2026-06-25 12:07:35 +0800

    [CI] [93677]  <platform> <fix> Use a new spinlock for tickless 2 (copy form http://192.168.0.46/gerrit/c/armino/bk_avdk_smp/ap/components/os_source/+/93398)

--- ap/components/os_source/freertos_smp_v2p0/FreeRTOS-Kernel/portable/GCC/ARM_CM55_NTZ/non_secure/port.c
+++ ap/components/os_source/freertos_smp_v2p0/FreeRTOS-Kernel/portable/GCC/ARM_CM55_NTZ/non_secure/port.c
@@ -604,6 +604,14 @@ static inline void systick_update(TickType_t xExpectedIdleTime, uint32_t ulReloa
 }
 #endif /* !CONFIG_UPDATE_TICK_THEN_ENABLE_INT */
 
+/* Dedicated spinlock for the tickless low-power (sleep/wake) sequence.
+ * Used instead of the kernel lock so this path acquires ONLY a cross-core
+ * spinlock without raising BASEPRI: local interrupts are masked via PRIMASK
+ * ("cpsid i"), so BASEPRI stays 0 across sleep/wake and the wake-window
+ * "cpsie i" can fully re-enable interrupts. Must use the spinlock_acquire/
+ * release initializer (not SPIN_LOCK_INIT). */
+static SPINLOCK_SECTION volatile spinlock_t pm_sleep_spin_lock = SPINLOCK_ACQUIRE_INITIALIZER;
+
 void vPortSuppressTicksAndSleep( TickType_t xExpectedIdleTime )
 {
     uint32_t ulReloadValue;
@@ -628,13 +636,16 @@ void vPortSuppressTicksAndSleep( TickType_t xExpectedIdleTime )
         ulReloadValue -= ulStoppedTimerCompensation;
     }
 
-    /* we can use taskENTER_CRITICAL() to enter a critical section, because
-    in critical section also use primask to mask interrupts,this will not
-    destory the existing logic.  */
+    /* Sleep-entry window: local interrupts are masked with PRIMASK ("cpsid i"),
+     * so we only need cross-core mutual exclusion here. Take the dedicated PM
+     * spinlock directly instead of prvTakeKernelLock(), which would also raise
+     * BASEPRI to configMAX_SYSCALL_INTERRUPT_PRIORITY. Keeping BASEPRI at 0
+     * across sleep/wake lets the wake-window "cpsie i" fully re-enable
+     * interrupts without a manual BASEPRI clear. */
     __asm volatile ( "cpsid i" ::: "memory" );
     __asm volatile ( "dsb" );
     __asm volatile ( "isb" );
-    prvTakeKernelLock();
+    spinlock_acquire(&pm_sleep_spin_lock, portMUX_NO_TIMEOUT);
 
     /* If a context switch is pending or a task is waiting for the scheduler
     * to be un-suspended then abandon the low power entry. */
@@ -653,7 +664,7 @@ void vPortSuppressTicksAndSleep( TickType_t xExpectedIdleTime )
         /* Re-enable interrupts - see comments above the cpsid instruction()
         * above. */
         // __asm volatile ( "cpsie i" ::: "memory" );
-        prvReleaseKernelLock();
+        spinlock_release(&pm_sleep_spin_lock, 0UL);
         __asm volatile ( "cpsie i" ::: "memory" );
 
     } else {
@@ -675,20 +686,20 @@ void vPortSuppressTicksAndSleep( TickType_t xExpectedIdleTime )
         * so a copy is taken. */
         xModifiableIdleTime = xExpectedIdleTime;
         configPRE_SLEEP_PROCESSING( xModifiableIdleTime );
-
+        spinlock_release(&pm_sleep_spin_lock, 0UL);
         if (xModifiableIdleTime > 0) {
-            prvReleaseKernelLock();
 #if CONFIG_PM
             pm_suspend(xModifiableIdleTime);
 #else
             bk_pm_suppress_ticks_and_sleep(xModifiableIdleTime);
 #endif
-            prvTakeKernelLock();
         }
 
         configPOST_SLEEP_PROCESSING( xExpectedIdleTime );
 #if CONFIG_UPDATE_TICK_THEN_ENABLE_INT
+        spinlock_acquire(&pm_sleep_spin_lock, portMUX_NO_TIMEOUT);
         systick_gated_update(xExpectedIdleTime, ulReloadValue);//it improve the systick update when adding here,otherwize the systick update fail and enter the vPortSuppressTicksAndSleep() fail
+        spinlock_release(&pm_sleep_spin_lock, 0UL);
 #endif
         /* Re-enable interrupts to allow the interrupt that brought the MCU
         * out of sleep mode to execute immediately. See comments above
@@ -705,16 +716,15 @@ void vPortSuppressTicksAndSleep( TickType_t xExpectedIdleTime )
         __asm volatile ( "dsb" );
         __asm volatile ( "isb" );
 #if CONFIG_UPDATE_TICK_THEN_ENABLE_INT
+        spinlock_acquire(&pm_sleep_spin_lock, portMUX_NO_TIMEOUT);
         systick_gated_update(xExpectedIdleTime, ulReloadValue);//it improve the systick update when adding here,otherwize the systick update fail and enter the vPortSuppressTicksAndSleep() fai
+        spinlock_release(&pm_sleep_spin_lock, 0UL);
 #else
         systick_update(xExpectedIdleTime, ulReloadValue);
 #endif
 
         /* Restart SysTick. */
         portNVIC_SYSTICK_CTRL_REG |= portNVIC_SYSTICK_ENABLE_BIT;
-        /* Exit with interrupts enabled. we used the critical to block interruption,
-        so should also be used accordingly here*/
-        prvReleaseKernelLock();
         __asm volatile ( "cpsie i" ::: "memory" );
     }
 }
