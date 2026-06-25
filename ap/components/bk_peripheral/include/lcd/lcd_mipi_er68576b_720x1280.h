2dd1154867357d22389c91d729f6256f317db171
itester <itester@aclsemi.com>
2026-06-25 11:10:38 +0800

    [CI] [93632]  <multimedia> <perf> change new 720p lcd

---
+++ ap/components/bk_peripheral/include/lcd/lcd_mipi_er68576b_720x1280.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <components/bk_lcd_panel.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if CONFIG_LCD_ER68576B_MIPI_720x1280
+extern const bk_display_dsi_panel_t lcd_device_er68576b_mipi_720x1280;
+#endif
+
+#ifdef __cplusplus
+}
+#endif
