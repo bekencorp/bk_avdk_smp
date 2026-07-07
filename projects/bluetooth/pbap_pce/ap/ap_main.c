91ebc69a512da9e8314f9fb22ca4b1d3e3aff33b
itester <itester@aclsemi.com>
2026-07-07 11:10:49 +0800

    [CI] [95508]  <bt> <feat> project pbap demo support

---
+++ projects/bluetooth/pbap_pce/ap/ap_main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "bk_private/bk_init.h"
+#include <components/system.h>
+#include <os/os.h>
+#include <components/shell_task.h>
+#include "components/bluetooth/bk_dm_bluetooth.h"
+
+#include "bt_manager.h"
+#include "pbap_pce_user_config.h"
+
+#define AUTO_ENABLE_BLUETOOTH_DEMO 1
+
+int main(void)
+{
+	bk_init();
+
+#if AUTO_ENABLE_BLUETOOTH_DEMO
+	uint8_t bt_mac[6] = {0};
+	char local_name[30] = {0};
+	bk_err_t err = bk_bluetooth_get_address(bt_mac);
+	if (err == BK_OK)
+	{
+		snprintf(local_name,
+				 sizeof(local_name),
+				 "%s_%02x%02x%02x",
+				 LOCAL_NAME,
+				 bt_mac[2],
+				 bt_mac[1],
+				 bt_mac[0]);
+	}
+	else
+	{
+		snprintf(local_name, sizeof(local_name), "%s", LOCAL_NAME);
+	}
+
+	bt_manager_cfg_t bt_manager_cfg =
+	{
+		.local_name = local_name,
+		.device_class = COD_PHONE,
+		.page_scan_interval = PAGE_SCAN_INTV,
+		.page_scan_window = PAGE_SCAN_WIN,
+		.page_timeout = CONFIG_PAGE_TIMEOUT,
+		.reconnect_interval_ms = CONFIG_RECONN_INTERVAL,
+		.max_reconnect_count = CONFIG_MAX_RECONN_COUNT,
+		.io_capability = BK_BT_IO_CAP_NONE,
+	};
+	bt_manager_init(&bt_manager_cfg);
+	bt_manager_set_mode(BT_MNG_MODE_PAIRING);
+
+#if CONFIG_BT && CONFIG_PBAP_PCE_DEMO
+	extern int cli_pbap_pce_demo_init(void);
+	cli_pbap_pce_demo_init();
+#endif
+#endif
+
+	return 0;
+}
