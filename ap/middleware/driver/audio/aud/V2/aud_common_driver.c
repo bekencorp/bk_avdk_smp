407a68c03d81f154f685fadb6a314141e78bcd9e
itester <itester@aclsemi.com>
2026-07-16 21:29:53 +0800

    [CI] [96266]  <multimedia> <perf> Optimize aud driver code.

--- ap/middleware/driver/audio/aud/V2/aud_common_driver.c
+++ ap/middleware/driver/audio/aud/V2/aud_common_driver.c
@@ -440,6 +440,7 @@ bk_err_t bk_aud_driver_init(void)
 
 	//bk_pm_module_vote_power_ctrl(PM_POWER_SUB_MODULE_NAME_AUDP_AUDIO, PM_POWER_MODULE_STATE_ON);
 	sys_drv_aud_select_clock(0);
+	sys_drv_apll_en(1);
 	//bk_pm_clock_ctrl(PM_CLK_ID_AUDIO, CLK_PWR_CTRL_PWR_UP);
 
 #if CONFIG_SOC_BK7259
@@ -535,6 +536,7 @@ bk_err_t bk_aud_driver_deinit(void)
 #endif
 
 #endif
+	//bk_aud_clk_deconfig();
 
 	//bk_pm_clock_ctrl(PM_CLK_ID_AUDIO, CLK_PWR_CTRL_PWR_DOWN);
 	//bk_pm_module_vote_power_ctrl(PM_POWER_SUB_MODULE_NAME_AUDP_AUDIO, PM_POWER_MODULE_STATE_OFF);
