377d3164f2b97e5f9e38b86940f54b125f520b0f
itester <itester@aclsemi.com>
2026-07-25 17:48:45 +0800

    [CI] [97359]  <wifi> <fix> optimize wifi and network api

---
+++ ap/components/bk_netif/include/bk_netif.h
@@ -0,0 +1,85 @@
+// Copyright 2020-2021 Beken
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <common/bk_err.h>
+#include <components/netif_types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief BK Netif internal API
+ * @defgroup bk_api_netif_internal BK Netif Internal API
+ * @{
+ */
+
+/**
+ * @brief  Initialize netif component (SDK internal)
+ *
+ * Registers the Wi-Fi event callback used to bring up STA/AP lwIP netifs.
+ * Called automatically from ``bk_init()`` via ``app_wifi_init()``; applications
+ * do not need to call this API.
+ *
+ * @return
+ *   - BK_OK: success
+ *   - others: failure
+ */
+bk_err_t bk_netif_init(void);
+
+/**
+ * @brief  Set local netif interface's IP4 address information
+ *
+ * This function only updates the local IP configuration and does not synchronize
+ * the configuration to a remote CPU.
+ *
+ * @param ifx The interface index
+ * @param config the IP4 information of the interface
+ *
+ * @return
+ *   - BK_OK: succeed
+ *   - BK_ERR_NETIF_IF: invalid netif interface ID
+ *   - BK_ERR_NULL_PARAM: config is NULL
+ */
+bk_err_t bk_netif_set_ip4_config_local(netif_if_t ifx, const netif_ip4_config_t *config);
+
+/**
+ * @brief  Switch STA netif IP mode to DHCP (internal / debug use)
+ *
+ * This API only updates the STA IP mode setting to DHCP via ip_address_set().
+ * It does not start the lwIP DHCP client; STA normally obtains an IP automatically
+ * via sta_ip_start() when Wi-Fi connects.
+ *
+ * @attention Typical applications do not need to call this API. It is mainly used
+ *            by the ``dhcpc`` CLI command to switch back to DHCP after a static IP
+ *            was configured. If the interface is already up with a static IP, stop
+ *            or restart the STA interface after calling this API.
+ *
+ * @param ifx  netif interface ID, currently only NETIF_IF_STA is supported.
+ *
+ * @return
+ *   - BK_OK: succeed
+ *   - BK_ERR_NETIF_IF: invalid netif interface ID
+ */
+bk_err_t bk_netif_dhcpc_start(netif_if_t ifx);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
