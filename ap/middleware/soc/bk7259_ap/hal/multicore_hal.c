e0ec671dfbd5e4dd6400301e906bae04428b5920
itester <itester@aclsemi.com>
2026-08-04 09:37:44 +0800

    [CI] [98452]  <platform> <fix> return success after stopping CPU2

--- ap/middleware/soc/bk7259_ap/hal/multicore_hal.c
+++ ap/middleware/soc/bk7259_ap/hal/multicore_hal.c
@@ -159,7 +159,7 @@ bk_err_t multicore_hal_stop(uint32_t id)
 		sys_ahbp_ll_set_reg4_cpu0_wfe_pulse(0);
 		sys_ahbp_ll_set_reg4_cpu0_wait(0);
 		sys_ahbp_ll_set_reg4_cpu0_sw_rstn(0);
-		return BK_ERR_NOT_SUPPORT;
+		break;
 	default:
 		return BK_ERR_PARAM;
 	}
