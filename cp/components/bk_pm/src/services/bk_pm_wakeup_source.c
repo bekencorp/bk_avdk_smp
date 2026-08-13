1160b19f397ef7195e988dd031b3f7d79f724802
itester <itester@aclsemi.com>
2026-08-13 20:44:59 +0800

    [CI] [99682]  <pm> <perf>Revert VAD configuration

--- cp/components/bk_pm/src/services/bk_pm_wakeup_source.c
+++ cp/components/bk_pm/src/services/bk_pm_wakeup_source.c
@@ -218,12 +218,14 @@ void pm_touched_wakeup_low_voltage()
 
 bk_err_t pm_vad_wakeup_low_voltage(void)
 {
-	return bk_lp_vad_set_sleep_para_before_sleep();
+	//return bk_lp_vad_set_sleep_para_before_sleep();
+	return BK_OK;
 }
 
 bk_err_t pm_vad_wakeup_deep_sleep(void)
 {
-	return bk_lp_vad_set_sleep_para_before_sleep();
+	//return bk_lp_vad_set_sleep_para_before_sleep();
+	return BK_OK;
 }
 void pm_rtc_wakeup_deep_sleep()
 {
