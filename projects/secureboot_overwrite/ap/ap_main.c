d9694e4f230336998fb149c53f76962133fc0b34
itester <itester@aclsemi.com>
2026-08-18 11:55:29 +0800

    [CI] [99606]  <security> <feat> add bk7259 secureboot_overwrite project (compressed OTA v1)

---
+++ projects/secureboot_overwrite/ap/ap_main.c
@@ -0,0 +1,14 @@
+// Copyright 2025 Beken
+//
+// secureboot_xip - AP NS application entry.
+
+#include "bk_private/bk_init.h"
+#include <components/system.h>
+#include <components/log.h>
+
+int main(void)
+{
+	bk_init();
+
+	return 0;
+}
