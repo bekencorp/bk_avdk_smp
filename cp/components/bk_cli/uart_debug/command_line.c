72395d90caec05e941195c5a23683084343261b1
itester <itester@aclsemi.com>
2026-08-18 11:51:49 +0800

    [CI] [99542,99543]  <wifi> <refactor> remove flash protect toggling in wifi cli

--- cp/components/bk_cli/uart_debug/command_line.c
+++ cp/components/bk_cli/uart_debug/command_line.c
@@ -4,11 +4,7 @@
 #include <os/str.h>
 #include "bk_uart.h"
 #include <os/mem.h>
-#if CONFIG_FLASH_ORIGIN_API
-#include "BkDriverFlash.h"
-#else
 #include <driver/flash_partition.h>
-#endif
 
 #include "bk_sys_ctrl.h"
 #include <components/system.h>
@@ -611,11 +607,7 @@ static int bkreg_run_command_implement(const char *content, int cnt)
 
 		rx_param        = (REGISTER_PARAM *)pHCIrxBuf->param;
 
-#if CONFIG_FLASH_ORIGIN_API
-		bk_logic_partition_t *pt = bk_flash_get_info(BK_PARTITION_SYS_RF);
-#else
 		bk_logic_partition_t *pt = bk_flash_partition_get_info(BK_PARTITION_SYS_RF);
-#endif
 		len_left = rx_param->value;
 		addr = rx_param->addr - pt->partition_start_addr;//0xFA000;
 
