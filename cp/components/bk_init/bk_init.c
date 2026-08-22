5538c39b8d432de5a32a8405e36f677534e72f41
itester <itester@aclsemi.com>
2026-08-22 23:24:30 +0800

    [CI] [100309]  <platform> <fix> register cli wifi event cbs after bk_event_init on CP

--- cp/components/bk_init/bk_init.c
+++ cp/components/bk_init/bk_init.c
@@ -16,6 +16,9 @@
 #include <modules/wifi.h>
 #include <components/netif.h>
 #include <components/event.h>
+#if (CONFIG_CLI)
+#include "cli.h"
+#endif
 #if CONFIG_WIFI_ENABLE
 #include "bk_netif.h"
 #endif
@@ -132,6 +135,9 @@ static int app_wifi_init(void)
 #if (CONFIG_WIFI_ENABLE)
 	wifi_init_config_t wifi_config = WIFI_DEFAULT_INIT_CONFIG();
 	BK_LOG_ON_ERR(bk_event_init());
+#if (CONFIG_CLI) && (CLI_CFG_WIFI == 1)
+	BK_LOG_ON_ERR(cli_wifi_register_event_cbs());
+#endif
 	BK_LOG_ON_ERR(bk_netif_init());
 	BK_LOG_ON_ERR(bk_wifi_init(&wifi_config));
 
