43e2fffa2d75255a8169c88e00829cad915efb57
itester <itester@aclsemi.com>
2026-09-08 19:46:17 +0800

    [CI] [102087]  <platform> <fix> keep OTA CLI registered in non-debug builds

--- ap/components/bk_cli/cli_main.c
+++ ap/components/bk_cli/cli_main.c
@@ -1599,14 +1599,6 @@ int bk_cli_init(void)
 
 #endif// CONFIG_CODE_SIZE_OPTIMIZE
 
-#if (CLI_CFG_MISC == 1)
-	cli_misc_init();
-#endif
-#if (CLI_CFG_OTA == 1)
-	cli_ota_init();
-#endif
-
-
 #if (CONFIG_PSA_MBEDTLS_TEST && CONFIG_PSA_MBEDTLS)
 	cli_psa_crypto_init();
 #endif
@@ -1620,6 +1612,14 @@ int bk_cli_init(void)
 #endif
 
 #endif //CONFIG_DEBUG_VERSION
+
+#if (CLI_CFG_MISC == 1)
+	cli_misc_init();
+#endif
+/* OTA CLI (http_ota / ab_version) must work in non-debug builds too. */
+#if (CLI_CFG_OTA == 1)
+	cli_ota_init();
+#endif
 
 /*-----open the cli comand both at release and debug vertion begin-----*/
 #if (CONFIG_PM_CLI_ENABLE == 1)
