87332813551fbecacbbba1c7f7d153aa99284b5c
itester <itester@aclsemi.com>
2026-09-14 17:04:14 +0800

    [CI] [102250]  <driver> <refactor> flash op-notify array registry impl

--- cp/middleware/driver/flash/flash_notify.c
+++ cp/middleware/driver/flash/flash_notify.c
@@ -18,24 +18,76 @@
 #include "flash_driver.h"
 // #include "mb_ipc_cmd.h"
 
-static void (*s_flash_op_notify)(uint32_t param) = NULL;
+#define FLASH_MAX_OP_NOTIFY_CNT (4)
 
-bk_err_t mb_flash_register_op_notify(void * notify_cb)
+typedef struct {
+	flash_op_notify_callback_t cb;
+	void *args;
+} flash_op_notify_slot_t;
+
+/* Array-based op-notify registry: the single registration path. */
+static flash_op_notify_slot_t s_flash_op_notify_slots[FLASH_MAX_OP_NOTIFY_CNT] = {0};
+
+bk_err_t mb_flash_register_op_notify_cb(flash_op_notify_callback_t notify_cb, void *args)
 {
-    s_flash_op_notify = (void (*)(uint32_t))notify_cb;
+	uint32_t i;
 
-	return BK_OK;
+	if (notify_cb == NULL)
+	{
+		return BK_ERR_PARAM;
+	}
+
+	/* Same callback already registered: just refresh its args. */
+	for (i = 0; i < FLASH_MAX_OP_NOTIFY_CNT; i++)
+	{
+		if (s_flash_op_notify_slots[i].cb == notify_cb)
+		{
+			s_flash_op_notify_slots[i].args = args;
+			return BK_OK;
+		}
+	}
+
+	for (i = 0; i < FLASH_MAX_OP_NOTIFY_CNT; i++)
+	{
+		if (s_flash_op_notify_slots[i].cb == NULL)
+		{
+			s_flash_op_notify_slots[i].cb = notify_cb;
+			s_flash_op_notify_slots[i].args = args;
+			return BK_OK;
+		}
+	}
+
+	return BK_ERR_FLASH_WAIT_CB_FULL;
 }
 
-bk_err_t mb_flash_unregister_op_notify(void * notify_cb)
+bk_err_t mb_flash_unregister_op_notify_cb(flash_op_notify_callback_t notify_cb)
 {
-	if(s_flash_op_notify == notify_cb)
+	uint32_t i;
+
+	for (i = 0; i < FLASH_MAX_OP_NOTIFY_CNT; i++)
 	{
-		s_flash_op_notify = NULL;
-		return BK_OK;
+		if (s_flash_op_notify_slots[i].cb == notify_cb)
+		{
+			s_flash_op_notify_slots[i].cb = NULL;
+			s_flash_op_notify_slots[i].args = NULL;
+			return BK_OK;
+		}
 	}
 
 	return BK_ERR_FLASH_WAIT_CB_NOT_REGISTER;
+}
+
+static void flash_op_notify_dispatch(uint32_t busy)
+{
+	uint32_t i;
+
+	for (i = 0; i < FLASH_MAX_OP_NOTIFY_CNT; i++)
+	{
+		if (s_flash_op_notify_slots[i].cb != NULL)
+		{
+			s_flash_op_notify_slots[i].cb(busy, s_flash_op_notify_slots[i].args);
+		}
+	}
 }
 
 bk_err_t mb_flash_ipc_init(void)
@@ -45,19 +97,17 @@ bk_err_t mb_flash_ipc_init(void)
 
 bk_err_t mb_flash_op_prepare(void)
 {
-	// disable the LCD dev interrupt.
-	if(s_flash_op_notify != NULL)
-		s_flash_op_notify(0);
+	/* notify every registered peripheral: flash is about to erase/write. */
+	flash_op_notify_dispatch(1);
 
 	return BK_OK;
 }
 
 bk_err_t mb_flash_op_finish(void)
 {
-	// enable the LCD dev interrupt.
-	if(s_flash_op_notify != NULL)
-		s_flash_op_notify(1);
-	
+	/* notify every registered peripheral: flash erase/write finished. */
+	flash_op_notify_dispatch(0);
+
 	return BK_OK;
 }
 
