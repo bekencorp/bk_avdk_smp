56f1185bb34638a0ea2f410b67519d96b008db16
itester <itester@aclsemi.com>
2026-09-14 18:56:11 +0800

    [CI] [102781]  <platform> <feat> add exception reboot info and bounded flush apis

--- ap/include/components/system.h
+++ ap/include/components/system.h
@@ -65,6 +65,18 @@ typedef enum {
     RESET_SOURCE_UNKNOWN = 0xff,
 } RESET_SOURCE_STATUS;
 
+typedef struct {
+    uint32_t primary_reason;
+    uint32_t secondary_reason;
+    uint32_t primary_core;
+    uint32_t secondary_core;
+    uint32_t pc;
+    uint32_t lr;
+    uint32_t sp;
+    uint32_t cfsr;
+    uint32_t hfsr;
+} bk_exception_reboot_info_t;
+
 typedef enum {
 	MAC_TYPE_BASE = 0,
 	MAC_TYPE_STA,
@@ -175,6 +187,7 @@ void bk_set_printf_port(uint8_t port_num);
 int bk_get_printf_port(void);
 uint32_t bk_misc_get_reset_reason(void);
 void bk_misc_set_reset_reason(uint32_t type);
+void bk_misc_persist_exception_reboot_info(const bk_exception_reboot_info_t *info);
 
 /**
  * @brief  GPIO LOG API
